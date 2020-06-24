109f9250e113de4e5e337357e0c6505d3a9bd9ef
EngMoPro <fightstart20000@yahoo.com>
2020-06-24 17:13:47 +0200

    a70q: add overlay and lineage-overlay

--- device.mk
+++ device.mk
@@ -1,3 +1,15 @@
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
+
+
+
 
 # Audio
 PRODUCT_PACKAGES += \
