ebe0a81eba8bc830042c50a894bd83a72729d2fb
EngMoPro <fightstart20000@yahoo.com>
2020-06-26 16:43:56 +0200

    add DRM HALS

--- device.mk
+++ device.mk
@@ -433,7 +433,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service    
     
-
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 
 
