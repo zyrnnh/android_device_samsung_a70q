9f319aeaa1799731d104853a2c5bb486ab01b813
EngMoPro <fightstart20000@yahoo.com>
2020-06-26 16:36:13 +0200

    lineage screen touch hal

--- device.mk
+++ device.mk
@@ -360,6 +360,38 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sm6150
+    
+    
+    
+# Touchscreen
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung \
+    libtinyxml2    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
