8ace067ad3614efd338923127947b3841e1d2a30
EngMoPro <fightstart20000@yahoo.com>
2020-06-26 16:35:25 +0200

    thermal

--- device.mk
+++ device.mk
@@ -355,7 +355,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext    
     
-
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.sm6150
 
 
 
