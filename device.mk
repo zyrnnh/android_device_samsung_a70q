08d2367b71805cf8ffd100c846b920af0b51e264
EngMoPro <fightstart20000@yahoo.com>
2020-06-26 16:37:21 +0200

    Virbrator

--- device.mk
+++ device.mk
@@ -375,7 +375,10 @@ PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service    
     
     
-    
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service    
     
     
     
